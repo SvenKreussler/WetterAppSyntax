9bcd8a28c5be5fd930455eea0a570b131966041f
Sven Kreußler <s02k08@icloud.com>
2023-11-20 14:07:30 +0100

    Core Data Macro Files entfernt Preview wieder lauffähig gemacht

---
+++ Wetter/Wetter/UI/Components/PillView.swift
@@ -0,0 +1,18 @@
+//
+//  PillView.swift
+//  Wetter
+//
+//  Created by Sven Kreußler on 20.11.23.
+//
+
+import SwiftUI
+
+struct PillView: View {
+    var body: some View {
+        Text("tet")
+    }
+}
+
+#Preview {
+    PillView()
+}
--- Wetter/Wetter/WetterApp.swift
+++ Wetter/Wetter/WetterApp.swift
@@ -2,19 +2,16 @@
 //  WetterApp.swift
 //  Wetter
 //
-//  Created by Sven Kreußler on 15.11.23.
+//  Created by Sven Kreußler on 20.11.23.
 //
 
 import SwiftUI
 
 @main
 struct WetterApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
